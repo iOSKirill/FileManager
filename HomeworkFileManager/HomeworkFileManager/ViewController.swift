4adb569db035ece0121302744497752523d42b44
ManuilenkoKirill <m.kirill4163@gmail.com>
2023-02-08 09:58:21 +0300

    initial homework18

---
+++ HomeworkFileManager/HomeworkFileManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeworkFileManager
+//
+//  Created by Kirill Manuilenko on 8.02.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
